44f1b1b0772ba36fdaa5cb2bbc0fd9b2cd038398
nmorken <noahmorken@gmail.com>
2021-12-23 13:19:57 -0700

    Add files via upload

---
+++ name/name_cases.py
@@ -0,0 +1,18 @@
+name = 'fRANK jAEGER'
+print(f"That ninja, Grey Fox, is {name.title()}?!")
+print(name)
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print(f"{name.title()} once said, \"A cornered fox is more dangerous than a jackal!\"")
+
+quote = "I will kill you or you will kill me... it makes no difference."
+message = f"{name.title()} also said, \"{quote}\""
+print(message)
+
+new_name = '  \tSolid Snake \n'
+print(new_name)
+print(new_name.rstrip())
+print(new_name.lstrip())
+print(new_name.strip())
